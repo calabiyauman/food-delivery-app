9de050c2079dc0ed79588159709ea1329ef09259
Düg <carnahandouglas@gmail.com>
2022-02-03 21:30:41 -0500

    save

--- requests/loadstores.js
+++ requests/loadstores.js
@@ -1,19 +1,34 @@
-const express = require('express')
-const axios = require('axios')
-const app = express()
+
+
+import React, {useState, useEffect} from "react";
+import axios from "axios"
+import
+
 import {APIURL, GET_STORE_LIST_NEAREST_CITY} from "./user_http_methods"
 //receive lat & long from customer device or input. or default the lat & long to center address.
 
-let Menurequest = () => {
-app.post("/", async (req, res) => {
+function StoreData() {
+    const [data, setData] = useState(
+        {StoreList: "value", {
+            Store1: "value", {
 
-        try{
-            const response = await axios.post(APIURL, GET_STORE_LIST_NEAREST_CITY)
-            alert(response.data)
+            },
+            Store2: 
         }
-        catch (err){
-            next(err)
         }
-})
+
+
+    );
+
 }
-export default Menurequest
+
+//listen for changes
+
+//update state
+// setState((prevState)=>({...prevState,team:[
+//     ...prevState.team,
+//     {
+//     name: "two",
+//      id: "2"
+//     }
+// ]}))
